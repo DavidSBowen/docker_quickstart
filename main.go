f804476e504681e38a9ed82b03fcfe068363abd3
lessthanjake328 <lessthanjake328@msn.com>
2019-05-05 16:13:39 -0400

    adds favicon, edits main.go to handle favicon

--- main.go
+++ main.go
@@ -8,13 +8,18 @@ import (
 func main() {
 	fmt.Println("Server listening")
 
-	http.HandleFunc("/", handlerHere)
+	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/favicon.ico", faviconHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
-func handlerHere(w http.ResponseWriter, r *http.Request) {
+func rootHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	w.Write([]byte("<h1>This is the page</h1>"))
 	w.Write([]byte("<h2>Read about this content</h2>"))
 	w.Write([]byte("<p>venenatis tellus in metus vulputate eu scelerisque felis imperdiet proin fermentum leo vel orci porta non pulvinar neque laoreet suspendisse</p>"))
+}
+
+func faviconHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "./favicon.ico")
 }
